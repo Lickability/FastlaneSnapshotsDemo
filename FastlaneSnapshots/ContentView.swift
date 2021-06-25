e3027c7d871303d36c5b94e6363a027e4f1f4f29
Daisy Ramos <daisyramos317@gmail.com>
2021-06-25 14:35:35 -0400

    Initial Commit

---
+++ FastlaneSnapshots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastlaneSnapshots
+//
+//  Created by Daisy Ramos on 6/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastlaneSnapshots/FastlaneSnapshotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneSnapshotsApp.swift
+//  FastlaneSnapshots
+//
+//  Created by Daisy Ramos on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneSnapshotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
